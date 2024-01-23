6f1f1a90e9c9bed8811b527289bc6bf9e45f2a16
Md. Rejoanur Rahman Apu <rej.ash11@gmail.com>
2024-01-24 04:09:21 +0600

    features: database integrated to backend

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Product,Order,Review,ShippingAddress,OrderItem
 # Register your models here.
+admin.site.register(Product)
+admin.site.register(OrderItem)
+admin.site.register(Order)
+admin.site.register(Review)
+admin.site.register(ShippingAddress)
