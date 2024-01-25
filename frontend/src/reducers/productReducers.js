ca5d1827a3ce1fda9f37ead0fd9dfd61d930d7ea
Md. Rejoanur Rahman Apu <rej.ash11@gmail.com>
2024-01-26 01:36:20 +0600

    features: implemented redux to the product-details page

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,4 +1,6 @@
-import { PRODUCT_LIST_FAIL,PRODUCT_LIST_REQUEST,PRODUCT_LIST_SUCCESS } from "../constants/productConstants";
+import { PRODUCT_LIST_FAIL,PRODUCT_LIST_REQUEST,PRODUCT_LIST_SUCCESS,
+    PRODUCT_DETAILS_SUCCESS,PRODUCT_DETAILS_FAIL,PRODUCT_DETAILS_REQUEST,
+    } from "../constants/productConstants";
 
 export  const productListReducers = (state = {products:[]},action)=>{
     switch(action.type){
@@ -7,6 +9,19 @@ export  const productListReducers = (state = {products:[]},action)=>{
         case PRODUCT_LIST_SUCCESS:
             return {loading:false,products:action.payload}
         case PRODUCT_LIST_FAIL:
+            return {loading:false, error:action.payload}
+        default :
+            return state
+    }
+}
+
+export  const productDetailsReducers = (state = {prod:{review:[]}},action)=>{
+    switch(action.type){
+        case PRODUCT_DETAILS_REQUEST:
+            return {loading:true, ...state}
+        case PRODUCT_DETAILS_SUCCESS:
+            return {loading:false,prod:action.payload}
+        case PRODUCT_DETAILS_FAIL:
             return {loading:false, error:action.payload}
         default :
             return state
