32959bd2012af00bf6446e4fd46eb76339c567e4
Mahfuz Twoha <mahfuzztwohaa@gmail.com>
2024-02-19 19:34:51 +0600

    add pagination frontend and backend also with api

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,45 +1,57 @@
-import axios from 'axios'
+import axios from "axios";
 
-import { PRODUCT_LIST_FAIL,PRODUCT_LIST_REQUEST,PRODUCT_LIST_SUCCESS,
-    PRODUCT_DETAILS_SUCCESS,PRODUCT_DETAILS_FAIL,PRODUCT_DETAILS_REQUEST,
+import {
+  PRODUCT_LIST_FAIL,
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_SUCCESS,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
+  PRODUCT_DETAILS_REQUEST,
 } from "../constants/productConstants";
 
-export const listProducts=(keyword='') => async (dispatch) => {
-    try{
-        dispatch({
-            type: PRODUCT_LIST_REQUEST
-        })
-        const {data} = await axios.get(`/api/products${keyword}`)
-        dispatch({
-            type:PRODUCT_LIST_SUCCESS,
-            payload: data
-        })
-    }
-    catch(error){
-        dispatch({
-            type: PRODUCT_LIST_FAIL,
-            payload: error.response && error.response.data.detail?
-            error.response.data.detail : error.message,
-        })
-    }
-}
+export const listProducts =
+  (page = 1, keyword = "") =>
+  async (dispatch) => {
+    try {
+      dispatch({
+        type: PRODUCT_LIST_REQUEST,
+      });
 
-export const listProductDetails=(id) => async (dispatch) => {
-    try{
-        dispatch({
-            type: PRODUCT_DETAILS_REQUEST
-        })
-        const {data} = await axios.get(`/api/products/${id}`)
-        dispatch({
-            type:PRODUCT_DETAILS_SUCCESS,
-            payload: data
-        })
+      // Update the URL to include the page parameter
+      const { data } = await axios.get(`/api/products/?page=${page}${keyword}`);
+
+      dispatch({
+        type: PRODUCT_LIST_SUCCESS,
+        payload: data,
+      });
+    } catch (error) {
+      dispatch({
+        type: PRODUCT_LIST_FAIL,
+        payload:
+          error.response && error.response.data.detail
+            ? error.response.data.detail
+            : error.message,
+      });
     }
-    catch(error){
-        dispatch({
-            type: PRODUCT_DETAILS_FAIL,
-            payload: error.response && error.response.data.detail?
-            error.response.data.detail : error.message,
-        })
-    }
-}
+  };
+
+export const listProductDetails = (id) => async (dispatch) => {
+  try {
+    dispatch({
+      type: PRODUCT_DETAILS_REQUEST,
+    });
+    const { data } = await axios.get(`/api/products/${id}`);
+    dispatch({
+      type: PRODUCT_DETAILS_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
+      payload:
+        error.response && error.response.data.detail
+          ? error.response.data.detail
+          : error.message,
+    });
+  }
+};
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,41 +1,63 @@
-import React,{useState,useEffect} from 'react'
-import {Row,Col} from 'react-bootstrap'
-import Product from '../components/Product'
-import axios from 'axios'
-import {useDispatch,useSelector} from 'react-redux'
-import {listProducts} from '../actions/productActions'
-import Loader from '../components/Loader'
-import ErrorMessage from '../components/ErrorMessage'
-import { useLocation, useNavigate } from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import { Row, Col } from "react-bootstrap";
+import Product from "../components/Product";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { listProducts } from "../actions/productActions";
+import Loader from "../components/Loader";
+import ErrorMessage from "../components/ErrorMessage";
+import { useLocation, useNavigate } from "react-router-dom";
 
-function HomeScreen({history}) { 
+import { Pagination } from "antd";
+
+function HomeScreen({ history }) {
   const dispatch = useDispatch();
-  const productList = useSelector(state=>state.productList)
-  const {error,loading,products} = productList
+  const productList = useSelector((state) => state.productList);
+  const { error, loading, products, currPage, totalPages, totalItems } =
+    productList;
 
   const location = useLocation();
   let keyword = location.search;
-  console.log(keyword)
 
-  useEffect(()=>{
-    dispatch(listProducts(keyword))
-  },[dispatch,keyword]) 
+  console.log(keyword);
+  console.log(products);
+
+  const [currentPage, setCurrentPage] = useState(1);
+
+  // Function to handle page change
+  const handleChange = (page) => {
+    setCurrentPage(page); // Update the current page state
+  };
+
+  useEffect(() => {
+    dispatch(listProducts(currentPage, keyword));
+  }, [dispatch, currentPage, keyword]);
 
   return (
     <div>
       <h1>Products</h1>
-      {loading? <Loader/>
-      :error?<ErrorMessage vant = 'danger' child = {error}></ErrorMessage>
-      :<Row>
-      {products.map(product=>(
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-              <Product product = {product}/>
-          </Col>
-      ))}
-    </Row>
-      }
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <ErrorMessage vant="danger" child={error}></ErrorMessage>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
+
+      <Pagination
+        onChange={handleChange} // Pass the handleChange function
+        defaultCurrent={currPage} // Set default current page to currentPage state
+        pageSize={totalPages} // Set the number of items per page
+        total={totalItems} // Set the total number of items (replace 100 with the actual count)
+      />
     </div>
-  )
+  );
 }
 
-export default HomeScreen
+export default HomeScreen;
